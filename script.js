18fce71199f941cda5a539d7266c21d62d01e797
Aymeric Neveu <emploi.neveu@gmail.com>
2018-10-09 13:51:13 +0200

    exercice4P2JS

---
+++ script.js
@@ -0,0 +1,5 @@
+function valide() {
+ var nom = document.getElementById('lastname').value='';
+ var prénom = document.getElementById('firstname').value='';
+ var ville = document.getElementById('city').value='';
+}
